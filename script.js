8b81d74e13269aab90d296ccbf86589503c0eda5
k-grit <kouji@k-grit.jp>
2026-09-04 11:20:41 +0900

    Add files via upload

---
+++ script.js
@@ -0,0 +1,7 @@
+const btn = document.querySelector('.menu-btn');
+const nav = document.querySelector('#navMenu');
+if (btn && nav) {
+	btn.addEventListener('click', () => nav.classList.toggle('open'));
+	nav.querySelectorAll('a').forEach(a => a.addEventListener('click', () => nav.classList
+		.remove('open')))
+}
